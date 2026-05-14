23d03c96b1b47567da7db7fa05fbac18795531a3
ZEMS <webpoka@gmail.com>
2026-05-14 18:37:52 +0600

    update

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -22,16 +22,6 @@ class HomeController extends Controller
 
         return view('Controllers/view/about', ['contents' => $contents]);
     }
-    public function allUser()
-    {
-        $users = [
-            new User('John Doe', '<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="a6ccc9cec8e6c3dec7cbd6cac388c5c9cb">sfsd</a>'),
-            new User('Jane Doe', '<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="fe949f909bbe9b869f938e929bd09d9193">sf</a>')
-        ];
-
-        $this->view('Controllers/view/user/index', ['users' => $users, 'test' => "Hello"]);
-    }
-
     public function service()
     {
         $users = Content::where('status', 1)->orWhere('type', 'news')->get();
